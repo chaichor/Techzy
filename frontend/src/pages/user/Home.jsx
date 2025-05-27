ac1d7669865fd06d11d95141bd5fc3980137db28
AngeMess <20230253@ricaldone.edu.sv>
2025-05-27 15:03:12 -0600

    sda

--- frontend/src/pages/user/Home.jsx
+++ frontend/src/pages/user/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import HeaderHome from '../../components/Home/headerHome';
-import InfoHome from '../../components/Home/infoHome';
+import InfoHome from '../../components/Home/InfoHome';
 import ProductCarousel from '../../components/productCarousel';
 import KeyboardCarousel from '../../components/keyboardCarousel';
 import KeycapsCarousel from '../../components/keycapsCarousel';
